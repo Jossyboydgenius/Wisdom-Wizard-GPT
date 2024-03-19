14ef91c928f01f13260b1b899c3d685e3372804f
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-19 18:02:11 +0000

    Wisdom-Wizard GPT

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        domain: "https://prepared-bobcat-36.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
