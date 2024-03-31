63717e63328676d6478f15aee9dbe2538a581dcc
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-31 03:25:43 +0000

    Wisdom Wizard GPT

---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+interface ConvexClientProviderProps {
+    children: React.ReactNode;
+};
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
