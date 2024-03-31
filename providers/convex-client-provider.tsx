ee1791f4a17b80f9651047551823ef10b339fa10
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-31 03:35:54 +0000

    Wisdom Wizard GPT

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { Authenticated, AuthLoading, ConvexReactClient, Unauthenticated } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
 interface ConvexClientProviderProps {
     children: React.ReactNode;
 };
@@ -7,3 +11,23 @@ interface ConvexClientProviderProps {
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
 
 const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+    children
+}: ConvexClientProviderProps) => {
+    return (
+        <ClerkProvider afterSignUpUrl="/sign-up">
+            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                <AuthLoading>
+                    <Loading />
+                </AuthLoading>
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <Unauthenticated>
+                    {children}
+                </Unauthenticated>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+}
