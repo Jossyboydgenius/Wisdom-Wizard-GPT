ebbaaffb7c5b04bcfed077ae96a9e24860ab335b
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-09 22:50:20 +0000

    Wisdom Wizard GPT

--- wwgpt/app/layout.tsx
+++ wwgpt/app/layout.tsx
@@ -31,7 +31,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ConvexClientProvider from "./ConvexClientProvider";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
