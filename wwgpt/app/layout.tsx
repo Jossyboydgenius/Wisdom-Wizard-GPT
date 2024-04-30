09cfa69515a614c0789977b000cc8d944e195ebb
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-30 23:33:14 +0000

    Wisdom Wizard GPT

--- wwgpt/app/layout.tsx
+++ wwgpt/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
-          {children}
+        {children}
         </ConvexClientProvider>
         </body>
     </html>
