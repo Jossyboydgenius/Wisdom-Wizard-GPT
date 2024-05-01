6733a0f835600c69fccef5f805c76673fcf2cc99
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-01 23:39:43 +0000

    Wisdom Wizard GPT

--- wwgpt/convex/_generated/api.d.ts
+++ wwgpt/convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as chats from "../chats.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,6 +27,7 @@ import type * as chats from "../chats.js";
  */
 declare const fullApi: ApiFromModules<{
   chats: typeof chats;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
