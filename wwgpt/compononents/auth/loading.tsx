e503954ad23e11e821c6699d567de4d2abb00c92
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-01 21:03:16 +0000

    Wisdom Wizard GPT

--- wwgpt/compononents/auth/loading.tsx
+++ wwgpt/compononents/auth/loading.tsx
@@ -2,16 +2,14 @@ import Image from "next/image"
 
 export const Loading = () => {
     return (
-        <main className="flex flex-col w-full h-full items-center justify-center bg-neutral-800">
+        <main className="flex flex-col w-full h-full items-center justify-center bg-black">
             <Image
             src={"/logo.svg"}
             alt="logo"
             width={600}
             height={600}
-            className="animate-pulse duration-700"
+            className="animate-pulse duration-700 m-auto"
             />
         </main>
     )
 }
-
-
