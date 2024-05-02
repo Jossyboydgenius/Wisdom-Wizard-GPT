3cfda55f5b2c85c51e6f0cd6e24d6dd3e6bae1cc
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-02 06:52:18 +0000

    Wisdom Wizard GPT

--- wwgpt/app/sign-up/page.tsx
+++ wwgpt/app/sign-up/page.tsx
@@ -1,33 +1,34 @@
 "use client";
 
-import { Loading } from "@/compononents/auth/loading";
-import { useEffect } from "react";
+import { Loading } from "@/components/auth/loading";
+import { api } from "@/convex/_generated/api";
 import { useConvexAuth, useMutation } from "convex/react";
 import { useRouter } from "next/navigation";
-import { api } from "@/convex/_generated/api";
+import { useEffect } from "react";
 
 const SignUp = () => {
     const { isAuthenticated } = useConvexAuth();
     const storeUser = useMutation(api.users.store);
     const router = useRouter();
 
-  useEffect(() => {
-    const storeUserData = async () => {
-      if (isAuthenticated) {
-        try {
-          await storeUser();
-          router.push("/");
-        } catch (error) {
-          console.log(error);
+    useEffect(() => {
+        const storeUserData = async () => {
+            if (isAuthenticated) {
+                try {
+                    await storeUser();
+                    router.push("/");
+                } catch (error) {
+                    console.log(error);
+                }
+            }
         }
-      }
-    };
-    storeUserData();
-  }, [isAuthenticated, storeUser, router]);
 
-  return (
-  <Loading />  
+        storeUserData();
+    }, [isAuthenticated, storeUser, router])
+
+    return (
+        <Loading />
     )
-};
+}
 
 export default SignUp;
