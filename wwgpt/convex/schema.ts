ae830095dff574cf74d3cacdb3278d9f3f228761
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-02 06:53:44 +0000

    Wisdom Wizard GPT

--- wwgpt/convex/schema.ts
+++ wwgpt/convex/schema.ts
@@ -1,21 +1,18 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-
 export default defineSchema({
     users: defineTable({
         tokenIdentifier: v.string(),
-        model: v.union(v.literal("gpt-3.5-turbo-1106"), v.literal(
-            "gpt-4-0125-preview"
-        )),
+        model: v.union(v.literal("gpt-3.5-turbo-1106"), v.literal("gpt-4-0125-preview")),
         endsOn: v.optional(v.number()),
         subscriptionId: v.optional(v.string()),
     })
         .index("by_token", ["tokenIdentifier"])
-        .index("by_subscription", ["subscriptionId"]),
+        .index("by_subscriptionId", ["subscriptionId"]),
     chats: defineTable({
         userId: v.id("users"),
         title: v.string(),
     })
-        .index("by_user", ["userId"])
+        .index("by_userId", ["userId"]),
 })
