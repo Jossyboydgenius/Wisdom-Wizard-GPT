519b7ec3c9e3a29df25f22517ed7441babbfb7e1
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-02 07:24:37 +0000

    Wisdom Wizard GPT

---
+++ wwgpt/middleware.ts
@@ -0,0 +1,23 @@
+// import { authMiddleware } from "@clerk/nextjs/server";
+
+// export default authMiddleware({
+//     //Routes that can be accessed while signed out
+//     publicRoutes: ['/'],
+//     // Routes that can always be accessed, and have
+//     // no authentication information
+//     ignoredRoutes: [],
+// });
+
+// export const config = {
+//     // Protects all routes, including api/trpc.
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
