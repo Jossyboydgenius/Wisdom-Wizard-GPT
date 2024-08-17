e018a81ff8fe2ee03db1ecaf96416b00ff51ff7e
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-17 10:10:29 +0000

    chore: Add chat functionality

--- wwgpt/convex/chats.ts
+++ wwgpt/convex/chats.ts
@@ -24,3 +24,44 @@ export const create = mutation({
         return chatId;
     },
 });
+
+export const list = query({
+    args: {},
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Called list chats without logged in user!");
+        }
+
+        const user = await ctx.db
+            .query("users")
+            .withIndex("by_token", (q) =>
+                q.eq("tokenIdentifier", identity.tokenIdentifier))
+            .unique();
+
+        if (user === null) {
+            throw new Error("User not found!");
+        }
+
+        return ctx.db
+            .query("chats")
+            .withIndex("by_userId", (q) => q.eq("userId", user._id))
+            .collect();
+    }
+});
+
+export const rename = mutation({
+    args: { id: v.id("chats"), title: v.string() },
+    handler: async (ctx, args) => {
+        await ctx.db.patch(args.id, {
+            title: args.title,
+        });
+    },
+});
+
+export const remove = mutation({
+    args: { id: v.id("chats") },
+    handler: async (ctx, args) => {
+        await ctx.db.delete(args.id);
+    },
+});
