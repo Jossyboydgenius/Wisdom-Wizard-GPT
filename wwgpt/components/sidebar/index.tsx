a437f5f49bd61f7f8c429313b9357f51823af0d6
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-17 10:12:19 +0000

    chore: Hide sidebar on smaller screens

--- wwgpt/components/sidebar/index.tsx
+++ wwgpt/components/sidebar/index.tsx
@@ -1,10 +1,11 @@
+import { ChatList } from "./chat-list";
 import { NewChatButton } from "./new-chat-button";
 
-export const Siderbar = () => {
+export const Sidebar = () => {
     return(
-        <div className="h-full lg:flex lg;flex-col lg:w-[300px] bg-neutral-950 p-4">
+        <div className="h-full hidden lg:flex lg:flex-col lg:w-[300px] bg-neutral-950 p-4">
             <NewChatButton />
-            ChatList
+            <ChatList />
             UpgradePlanButton
         </div>
     );
