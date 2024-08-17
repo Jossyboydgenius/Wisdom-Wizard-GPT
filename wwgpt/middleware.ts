39e64d39914ed80cb565f032411e352e1bbc1a85
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-17 10:11:10 +0000

    chore: Update middleware.ts with clerkMiddleware and authMiddleware

--- wwgpt/middleware.ts
+++ wwgpt/middleware.ts
@@ -1,23 +1,24 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
-
-// export default authMiddleware({
-//     //Routes that can be accessed while signed out
-//     publicRoutes: ['/'],
-//     // Routes that can always be accessed, and have
-//     // no authentication information
-//     ignoredRoutes: [],
-// });
-
-// export const config = {
-//     // Protects all routes, including api/trpc.
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+    //Routes that can be accessed while signed out
+    publicRoutes: ['/'],
+    // Routes that can always be accessed, and have
+    // no authentication information
+    ignoredRoutes: [],
+});
 
 export const config = {
+    // Protects all routes, including api/trpc.
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
