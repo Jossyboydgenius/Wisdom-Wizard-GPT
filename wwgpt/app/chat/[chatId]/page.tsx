63edd2832fea165838e381703294039e7a0250bc
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-18 09:40:32 +0000

    feat: Update chat page to use client-side rendering and add chat components

--- wwgpt/app/chat/[chatId]/page.tsx
+++ wwgpt/app/chat/[chatId]/page.tsx
@@ -1,7 +1,39 @@
-const Chat = () =>{
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { Header } from "./_components/header";
+import { Body } from "./_components/body";
+import { Form } from "./_components/form";
+
+interface ChatPageProps {
+    params: {
+        chatId: Id<"chats">;
+    }
+}
+
+const Chat = ({ params }: ChatPageProps) => {
+    const chat = useQuery(api.chats.get, { id: params.chatId });
+
+    const router = useRouter();
+
+    if (chat === null) {
+        router.push("/");
+    }
+
+
     return (
-        <div>
-            This is chat
+        <div className="bg-neutral-800 w-full h-full flex flex-col">
+            <Header />
+            <div className="flex flex-col h-full w-full">
+                <Body chatId={params.chatId} />
+                <div className="w-full fixed bottom-0">
+                    <Form chatId={params.chatId} />
+                    <p className="w-full text-center text-xs text-neutral-400 my-2 lg:pr-[300px]">Wisdom Wizard GPT can make mistakes. Consider checking important info.</p>
+                </div>
+            </div>
         </div>
     )
 }
