79b13f542c6fe8d68a19adbc85aff55c90a98ca7
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-18 09:41:30 +0000

    feat: Add user-related queries and mutations for subscription management

--- wwgpt/convex/users.ts
+++ wwgpt/convex/users.ts
@@ -35,3 +35,73 @@ export const store = mutation({
   },
 })
 
+export const currentUser = query({
+  args: {},
+  handler: async (ctx) => {
+      const identity = await ctx.auth.getUserIdentity();
+      if (!identity) {
+          throw new Error("Called selectGPT without authenticated user");
+      }
+
+      return await ctx.db
+          .query("users")
+          .withIndex("by_token", (q) =>
+              q.eq("tokenIdentifier", identity.tokenIdentifier))
+          .unique();
+  }
+})
+
+export const selectGPT = mutation({
+  args: { model: v.union(v.literal("gpt-3.5-turbo-1106"), v.literal("gpt-4-0125-preview")) },
+  handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+      if (!identity) {
+          throw new Error("Called selectGPT without authenticated user");
+      }
+
+      const user = await ctx.db
+          .query("users")
+          .withIndex("by_token", (q) =>
+              q.eq("tokenIdentifier", identity.tokenIdentifier))
+          .unique();
+
+      if (user === null) {
+          throw new Error("User not found");
+      }
+
+      await ctx.db.patch(user._id, {
+          model: args.model
+      });
+
+      return user._id;
+  }
+})
+
+//update subscription
+export const updateSubscription = internalMutation({
+  args: { subscriptionId: v.string(), userId: v.id("users"), endsOn: v.number() },
+  handler: async (ctx, { subscriptionId, userId, endsOn }) => {
+      await ctx.db.patch(userId, {
+          subscriptionId: subscriptionId,
+          endsOn: endsOn
+      });
+  },
+});
+
+//Update subscription by ID
+export const updateSubscriptionById = internalMutation({
+  args: { subscriptionId: v.string(), endsOn: v.number() },
+  handler: async (ctx, { subscriptionId, endsOn }) => {
+      const user = await ctx.db.query("users")
+          .withIndex("by_subscriptionId", (q) => q.eq("subscriptionId", subscriptionId))
+          .unique();
+
+      if (!user) {
+          throw new Error("User not found!");
+      }
+
+      await ctx.db.patch(user._id, {
+          endsOn: endsOn
+      });
+  },
+});
