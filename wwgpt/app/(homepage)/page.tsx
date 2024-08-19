7db38b448f967500c0992b1779dc648fc52056b0
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-08-19 21:57:41 +0000

    feat: Update homepage to create a new chat and redirect to chat page
    Refactor the homepage component to include a new functionality for creating a new chat. This functionality uses the useMutation hook from the Convex library to store the user data and obtain a chat ID. After successfully creating the chat, the user is redirected to the chat page using the useRouter hook from Next.js.
    
    This commit also includes some minor styling changes to the homepage component.

--- wwgpt/app/(homepage)/page.tsx
+++ wwgpt/app/(homepage)/page.tsx
@@ -1,7 +1,24 @@
-const Homepage =()=>{
-    return(
-        <div className="bg-neutral-800 h-full">Homepage!</div>
-    )
-}
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useMutation, useQueries, useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const Homepage = () => {
+    const storeUser = useMutation(api.users.store);
+    const router = useRouter();
+    useEffect(() => {
+        const fetch = async () => {
+            const chatId = await storeUser({});
+            router.push(`/chat/${chatId}`)
+        }
+        fetch();
+    }, [storeUser, router])
+
+    return (
+        <div className="bg-neutral-800 h-full text-neutral-400 text-3xl text-center px-11 pt-11">Creating a new chat</div>
+    );
+};
 
 export default Homepage;
